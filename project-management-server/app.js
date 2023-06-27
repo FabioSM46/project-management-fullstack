32a767c503f39e60d0cd14e6c072ebb419cc31b7
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-27 12:24:01 +0200

    protect routes

--- project-management-server/app.js
+++ project-management-server/app.js
@@ -8,7 +8,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+const { isAuthenticated } = require("../middleware/auth");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
@@ -20,9 +20,9 @@ app.use("/api", require("./routes/index.routes"));
 
 app.use("/auth", require("./routes/auth.routes"));
 
-app.use("/api", require("./routes/project.routes"));
+app.use("/api", isAuthenticated, require("./routes/project.routes"));
 
-app.use("/api", require("./routes/task.routes"));
+app.use("/api", isAuthenticated, require("./routes/task.routes"));
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
